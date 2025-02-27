f4a15b96b6ff3af0f4b3fe878f4159697a3426cc
amiramiour <amiouramirtahar@gmail.com>
2025-02-27 11:53:36 +0100

    update README.md

--- src/s6/e1/mon-module/index.ts
+++
@@ -1,9 +0,0 @@
-function alerter(message: string) {
-  console.log("Alerte:", message);
-}
-
-function logger(message: string) {
-  console.log("Log:", message);
-}
-
-export { alerter, logger };
--- src/s6/e1/testModule.ts
+++
@@ -1,5 +0,0 @@
-import { alerter, logger } from "./mon-module/index.js";
-
-console.log("--- S6 E1 ---");
-alerter("Hello World");
-logger("Hello World");
